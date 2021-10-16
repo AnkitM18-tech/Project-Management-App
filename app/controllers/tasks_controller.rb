cd951e699bdf3c91b42ab758f75b251a2b05a08f
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2021-10-16 20:23:16 +0530

    Added create and delete task functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,8 +3,18 @@ class TasksController < ApplicationController
     before_action :set_project
 
     def create
-        @task = @project.task.create(task_params)
+        @task = @project.tasks.create(task_params)
         redirect_to @project 
+    end
+
+    def destroy
+        @task = @project.tasks.find(params[:id])
+        if @task.destroy
+          flash[:success] = "Task deleted Successfully!"
+        else
+          flash[:error] = "Task deletion Failed!"
+        end
+        redirect_to @project
     end
 
     private
